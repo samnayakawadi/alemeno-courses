4fcdd3b11bd1e1d41988cd6b7a6810f732e0d89d
Samir Nayakawadi <samnayakawadi@gmail.com>
2023-12-01 22:09:25 +0530

    Course List UI Added

--- src/components/course-list/CourseList.jsx
+++ src/components/course-list/CourseList.jsx
@@ -1,6 +1,74 @@
+import SingleCourseCard from "./SingleCourseCard"
+
 const CourseList = () => {
+
+    const courses = [
+        {
+            courseName: "React Native",
+            courseDescription: "Build cross-platform mobile applications using React Native framework.",
+            courseInstructor: "Samir Nayakawadi"
+          },
+        {
+            courseName: "Machine Learning",
+            courseDescription: "Explore the world of machine learning with hands-on projects.",
+            courseInstructor: "Dr. Amanda Rodriguez"
+        },
+        {
+            courseName: "Web Development Bootcamp",
+            courseDescription: "Learn full-stack web development from scratch with industry experts.",
+            courseInstructor: "John Smith"
+        },
+        {
+            courseName: "Data Science Fundamentals",
+            courseDescription: "Gain a solid understanding of data science principles and techniques.",
+            courseInstructor: "Dr. Emily Davis"
+        },
+        {
+            courseName: "Mobile App Design",
+            courseDescription: "Design intuitive and user-friendly mobile applications.",
+            courseInstructor: "Lisa Johnson"
+        },
+        {
+            courseName: "JavaScript Mastery",
+            courseDescription: "Master JavaScript programming with real-world projects and challenges.",
+            courseInstructor: "Chris Anderson"
+        },
+        {
+            courseName: "Cybersecurity Essentials",
+            courseDescription: "Learn essential cybersecurity skills to protect digital assets.",
+            courseInstructor: "Alex Ramirez"
+        },
+        {
+            courseName: "Artificial Intelligence Basics",
+            courseDescription: "Introduction to the fundamentals of artificial intelligence and its applications.",
+            courseInstructor: "Dr. Michael Lee"
+        },
+        {
+            courseName: "Cloud Computing Fundamentals",
+            courseDescription: "Understand the basics of cloud computing and its role in modern IT.",
+            courseInstructor: "Sarah Thompson"
+        },
+        {
+            courseName: "UI/UX Design Principles",
+            courseDescription: "Create visually appealing and user-centric interfaces with design principles.",
+            courseInstructor: "Daniel Brown"
+        },
+    ];
+
     return (
-        <div className="text-center">CourseList</div>
+        <div>
+            <div className="flex justify-center gap-3 mt-3">
+                <input type="text" placeholder="Search Course" className="input input-bordered w-full max-w-xs" />
+                <button className="btn btn-success">Search</button>
+            </div>
+            <div className="grid grid-cols-4 gap-3 p-5 mt-5">
+                {
+                    courses.map((singleCourse, courseIndex) => {
+                        return <SingleCourseCard courseData={singleCourse} key={courseIndex} />
+                    })
+                }
+            </div>
+        </div>
     )
 }
 
---
+++ src/components/course-list/SingleCourseCard.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+const SingleCourseCard = ({ courseData }) => {
+    return (
+        <div className="card bg-base-100 shadow-xl">
+            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{courseData.courseName}</h2>
+                <p>{courseData.courseDescription}</p>
+                <p className="">by {courseData.courseInstructor}</p>
+                <div className="card-actions justify-end pt-3">
+                    <button className="btn btn-primary btn-block">View Course Details</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SingleCourseCard
